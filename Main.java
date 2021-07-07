8b0648a4f14545d0d095b10221f9f55e4920f15a
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-07-07 23:54:33 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+    String name = "Yuto";
+
+    int age = 26;
+
+    double height = 179.8;
+
+    double weight = 89.8;
+
+    double bmi = weight / height / height;
+
+    System.out.println("名前は" + name + "です。" );
+    System.out.println("年齢は" + age + "です。");
+    System.out.println("身長は" + height + "です。");
+    System.out.println("体重は" + weight + "です。");
+    System.out.println("BMIは" + bmi + "です。");
+  }
+
+}
