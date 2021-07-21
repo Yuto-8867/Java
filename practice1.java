bb4a01d4eca484c80b49e2ef40723011567408e3
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-07-21 21:39:12 +0900

    bmi

--- practice1.java
+++ practice1.java
@@ -1,5 +1,16 @@
 class Practice1 {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+    String name = "Yuto";
+    int age = 26;
+    double height = 179.8;
+    double weight = 89.8;
+    double bmi = weight / height / height;
+
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMI" + bmi + "です");
   }
 }
