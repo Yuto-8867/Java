abaa4afa4bc0de3a2cbb0f991742a1518d44edbb
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-07-26 21:50:20 +0900

    lesson5

--- practice4.java
+++ practice4.java
@@ -9,6 +9,15 @@ class practice4 {
     String lastName = scanner.next();
     System.out.print("年齢 :");
     int age = scanner.nextInt();
+    System.out.print("身長(m) :");
+    double height = scanner.nextDouble();
+    System.out.print("体重(kg) :");
+    double weight = scanner.nextDouble();
+
+    printData(firstName,lastName,age,height,weight);
+  }
+
+  public static void printData(String firstName, String lastName, int age, double height, double weight) {
     System.out.println("私の名前は" + fullName(firstName,lastName) + "です");
     System.out.println("私の年齢は" + age + "です");
     if(age >= 20) {
@@ -16,6 +25,8 @@ class practice4 {
     }else {
       System.out.println("未成年です");
     }
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
   }
 
   public static String fullName(String firstName,String lastName) {
