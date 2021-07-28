edc9b09600bfaabd2060aa8479356c62cc3337de
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-07-28 21:49:32 +0900

    lesson6 half

---
+++ Practice6.java
@@ -0,0 +1,10 @@
+class Practice6 {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate Jones");
+    person1.hello();
+
+    Person person2 = new Person("John Christopher Smith");
+    person2.hello();
+
+  }
+}
