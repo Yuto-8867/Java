5de3bafae770f594952d21c16187305f7153272f
YutoInoue <baseball.taki.yi.1994@gmail.com>
2021-08-01 22:13:19 +0900

    first commit

--- Person.java
+++ Person.java
@@ -1,36 +1,6 @@
 class Person {
-
-  public static int count = 0;
-  public String firstName;
-  public String lastName;
-  public int age;
-  public double height;
-  public double weight;
-
-  Person(String firstName, String lastName, int age, double height, double weight) {
-    this.count++;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-  }
-
-  public String fullName() {
-    return firstName + " " + lastName;
-  }
-
-  public double bmi() {
-    return weight / height / height;
-  }
-
-  public void printData() {
-    System.out.println("私の名前は" + this.fullName() + "です");
-    System.out.println("私の年齢は" + this.age + "です");
-    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
-  }
-
-  public static void printCount() {
-    System.out.println("合計は" + Person.count + "人です");
+  public void hello() {
+    // 「こんにちは」と出力されるように書き換えてください
+    System.out.println("Hello World");
   }
 }
--- Practice6.java
+++ Practice6.java
@@ -1,11 +1,6 @@
 class Practice6 {
   public static void main(String[] args) {
-    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
-    person1.printData();
-
-    Person person2 = new Person("John", "Smith", 65, 1.75, 80.0);
-    person2.printData();
-
-    Person.printCount();
+    Person person = new Person();
+    person.hello();
   }
 }
